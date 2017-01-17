40f3eb7656683205b43af2ce5ff81edbc994257f
Genevieve Block <Genevieve.block@my.simpson.edu>
2017-01-17 14:15:08 -0600

    My own Project

---
+++ Sample Programs/Drawing Sample.py
@@ -0,0 +1,6 @@
+# This is my Drawing program
+"""This is a sample program to show how to draw using python programming language and the Arcade library.
+Multi-line comments are surrounded by three of the double-quote marks. Single-line comments start with a hash/popund sign. #"""
+import arcade
+
+
