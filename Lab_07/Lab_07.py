fbfe4778bdbc6642aff1b625ff0c24a2f54e0952
Genevieve Block <Genevieve.block@my.simpson.edu>
2017-03-03 11:18:58 -0600

    Changes made to laser (1).ogg

--- Lab_07/Lab_07.py
+++ Lab_07/Lab_07.py
@@ -8,7 +8,6 @@ MOVEMENT_SPEED = 3
 
 def draw_whale(position_x,position_y,):
 
-
     arcade.draw_circle_filled(position_x - 100, position_y - 195, 80, arcade.color.PALE_AQUA)
     arcade.draw_rectangle_filled(position_x, position_y - 165.5, 230, 100, arcade.color.PALE_AQUA)
     arcade.draw_circle_filled(position_x + 100, position_y - 195, 80, arcade.color.PALE_AQUA)
@@ -20,14 +19,12 @@ def draw_whale(position_x,position_y,):
 
 def draw_ocean(position_x, position_y,):
 
-
     arcade.draw_rectangle_filled(position_x, position_y + 190, 2000, 100, arcade.color.MEDIUM_BLUE)
     arcade.draw_rectangle_filled(position_x, position_y + 90, 2000, 100, arcade.color.MEDIUM_ELECTRIC_BLUE)
     arcade.draw_rectangle_filled(position_x, position_y + 0, 2000, 100, arcade.color.MEDIUM_PERSIAN_BLUE)
 
 
 def draw_sunset(position_x, position_y):
-
 
     arcade.draw_circle_filled(position_x - 400, position_y - 200, 500, arcade.color.AMARANTH_PINK)
     arcade.draw_circle_filled(position_x - 400, position_y - 200, 400, arcade.color.RAJAH)
@@ -36,7 +33,9 @@ def draw_sunset(position_x, position_y):
     arcade.draw_circle_filled(position_x - 400, position_y - 200, 100, arcade.color.SAFETY_ORANGE)
     arcade.draw_circle_filled(position_x - 400, position_y - 200, 50, arcade.color.YELLOW)
 
+
 class Ball:
+
     def __init__(self, position_x, position_y, change_x, change_y, radius, color):
 
         self.position_x = position_x
@@ -45,7 +44,7 @@ class Ball:
         self.change_y = change_y
         self.radius = radius
         self.color = color
-        self.sound = arcade.load_sound("laser(1).ogg")
+        self.sound = arcade.load_sound("laser (1).ogg")
 
     def draw(self):
 
@@ -82,11 +81,11 @@ class Square:
         self.change_y = change_y
         self.SQUARE_CENTER = SQUARE_CENTER
         self.color = color
-        self.sound = arcade.load_sound("laser(1).ogg")
+        self.sound = arcade.load_sound("laser (1).ogg")
 
     def draw(self):
-
         arcade.draw_rectangle_filled(self.position_x, self.position_y, 40, self.SQUARE_CENTER, self.color)
+
     def animate(self):
         if self.position_x < self.SQUARE_CENTER / 2:
             self.position_x = self.SQUARE_CENTER / 2
@@ -125,7 +124,7 @@ class MyApplication(arcade.Window):
         #create the square
         self.square = Square(550,50,40,40,SQUARE_CENTER,arcade.color.PALE_AQUA)
 
-        self.laser_sound = arcade.load_sound("laser(1).ogg")
+        self.laser_sound = arcade.load_sound("laser (1).ogg")
     def animate(self, delta_time):
         self.ball.animate()
 
@@ -146,6 +145,7 @@ class MyApplication(arcade.Window):
             self.ball.change_x = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
             self.ball.change_y = 0
+
     def on_draw(self):
 
         arcade.start_render()
